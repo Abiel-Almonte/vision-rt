006cdd6d82b1ed5ab3044edb28d7c27cd880c6d8
Abiel Almonte <almonteabiel@gmail.com>
2025-12-25 20:07:48 -0500

    Add example usage

---
+++ usage.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from torchvision.models import resnet50, ResNet50_Weights
+
+import visionrt
+from visionrt import Camera
+
+visionrt.config.verbose = True
+visionrt.config.optims.fold_conv_bn = True
+visionrt.config.cudagraphs = True
+
+model = (
+    nn.Sequential(
+        nn.Upsample(size=(224, 224), mode="bilinear", align_corners=False),
+        resnet50(weights=ResNet50_Weights.IMAGENET1K_V2),
+        nn.Softmax(dim=1),
+    )
+    .cuda()
+    .eval()
+)
+labels = ResNet50_Weights.IMAGENET1K_V2.meta["categories"]
+
+optimized_model = visionrt.compile(model)
+print(optimized_model)  # snapshot of the config
+
+with torch.inference_mode():
+    camera = Camera("/dev/video0")
+    for frame in camera.stream():
+        frame = frame.unsqueeze(0)
+
+        out = optimized_model(frame)
+
+        pred_class = out.argmax(dim=1).item()
+        label = labels[pred_class]
+        # print(f"Predicted class: {label}")
+        break
+
+print(optimized_model)  # cuda graph status should change
